72150872df0f9d5c476719df4b26747f33335d7c
EightDoor <851708184@qq.com>
2021-04-23 17:59:23 +0800

    fix

--- lib/utils/upgrade_dialog.dart
+++ lib/utils/upgrade_dialog.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutterbbase/config/config.dart';
 import 'package:flutterbbase/models/upgrade.dart';
 import 'package:flutterbbase/utils/index.dart';
+import 'package:get/get.dart';
+import 'package:r_upgrade/r_upgrade.dart';
+
+GlobalKey<_UpgradeDialogState> globalKeyUpgrade =
+    GlobalKey<_UpgradeDialogState>();
 
 class UpgradeDialog extends StatefulWidget {
   final Upgrade upgrade;
@@ -13,6 +19,7 @@ class UpgradeDialog extends StatefulWidget {
 
 class _UpgradeDialogState extends State<UpgradeDialog> {
   late Data data;
+  double progress = 0;
   @override
   void initState() {
     super.initState();
@@ -33,30 +40,80 @@ class _UpgradeDialogState extends State<UpgradeDialog> {
         color: Colors.white,
       ),
       child: data.buildFileSize != null
-          ? ListView(
+          ? Stack(
               children: [
-                ListTile(
-                  leading: Text("大小"),
-                  title: Text(
-                    (int.parse(data.buildFileSize) / 1024 / 1024)
-                            .round()
-                            .toString() +
-                        " M",
-                  ),
-                ),
-                ListTile(
-                  leading: Text("版本号"),
-                  title: Text(
-                    data.buildVersion,
-                  ),
-                ),
-                ListTile(
-                  leading: Text("应用更新说明"),
-                ),
-                Text(data.buildUpdateDescription)
+                ListView(
+                  children: [
+                    ListTile(
+                      leading: Text("大小"),
+                      title: Text(
+                        (int.parse(data.buildFileSize) / 1024 / 1024)
+                                .round()
+                                .toString() +
+                            " M",
+                      ),
+                    ),
+                    ListTile(
+                      leading: Text("版本号"),
+                      title: Text(
+                        data.buildVersion,
+                      ),
+                    ),
+                    ListTile(
+                      leading: Text("应用更新说明"),
+                    ),
+                    // 模糊进度条(会执行一个动画)
+                    LinearProgressIndicator(
+                      backgroundColor: Colors.grey[200],
+                      value: this.progress,
+                      valueColor: AlwaysStoppedAnimation(Colors.blue),
+                    ),
+                    Text(data.buildUpdateDescription),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      width: .8.sw,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SimpleDialogOption(
+                            child: Text("更新"),
+                            onPressed: () {
+                              Utils.log().i("更新");
+                              isDownload();
+                            },
+                          ),
+                          SimpleDialogOption(
+                            child: Text("取消"),
+                            onPressed: () {
+                              Get.back();
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                )
               ],
             )
           : Container(),
     );
+  }
+
+  /// 开始下载应用
+  static void isDownload() async {
+    int? id = await RUpgrade.upgrade(UpgradeSetting.downloadUrl);
+    if (id != null) {
+      install(id);
+    }
+  }
+
+  /// 下载应用
+  static void install(int id) async {
+    await RUpgrade.install(id);
+    RUpgrade.stream.listen((event) {
+      Utils.log().i(event.percent);
+      globalKeyUpgrade.currentState!.progress = event.percent!;
+    });
   }
 }
--- lib/utils/upgrade_util.dart
+++ lib/utils/upgrade_util.dart
@@ -2,17 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutterbbase/api/api_common.dart';
-import 'package:flutterbbase/config/config.dart';
 import 'package:flutterbbase/models/upgrade.dart';
 import 'package:flutterbbase/utils/index.dart';
-import 'package:flutterbbase/utils/upgrade_dialog.dart';
-import 'package:get/get.dart';
 import 'package:package_info/package_info.dart';
-import 'package:r_upgrade/r_upgrade.dart';
-
-GlobalKey globalKeyUpgrade = GlobalKey();
 
 class UpgradeUtil {
   /// 升级
@@ -61,47 +54,9 @@ class UpgradeUtil {
     var result = await showDialog(
       context: context,
       builder: (context) {
-        return SimpleDialog(
-          key: globalKeyUpgrade,
-          title: Center(
-            child: Text(upgrade.data.buildName),
-          ),
-          children: [
-            Stack(
-              children: [
-                UpgradeDialog(upgrade: upgrade),
-                // Positioned(
-                //   top: 0,
-                //   left: 0,
-                //   child:
-                //       WaveProgress(180.0, Colors.blue, Colors.blueAccent, 50),
-                // ),
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  width: .8.sw,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      SimpleDialogOption(
-                        child: Text("更新"),
-                        onPressed: () {
-                          Utils.log().i("更新");
-                          isDownload();
-                        },
-                      ),
-                      SimpleDialogOption(
-                        child: Text("取消"),
-                        onPressed: () {
-                          Get.back();
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            )
-          ],
+        return Dialog(
+          child: Text("123"),
+          // UpgradeDialog(upgrade: upgrade)
         );
       },
     );
@@ -109,21 +64,5 @@ class UpgradeUtil {
       Utils.log().i("点击了关闭: " + result);
     }
     // isDownload();
-  }
-
-  /// 开始下载应用
-  static void isDownload() async {
-    int? id = await RUpgrade.upgrade(UpgradeSetting.downloadUrl);
-    if (id != null) {
-      install(id);
-    }
-  }
-
-  /// 下载应用
-  static void install(int id) async {
-    await RUpgrade.install(id);
-    RUpgrade.stream.listen((event) {
-      Utils.log().i(event.percent);
-    });
   }
 }
